fb6f060ae80f43136823af9066abf7373a17b1c3
Goldie <goldiejackson25@yahoo.com>
2021-11-24 05:01:39 -0600

    commit tree fbf8c7b0a5472a49b74cec6faff71b036d0f7e74

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,15 @@
+require "pry"
+
 class Dog
+    attr_accessor :name, :breed
+
+    def initialize(names, breeds)
+        @name = names
+        @breed = breeds
+    end
 
 end
+
+d1 = Dog.new("Dust Bunny", "Morkie")
+
+binding.pry
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,12 @@
 class Person
 
+    attr_accessor :name, :job
+
+    def initialize(name, job)
+        @name = name
+        @job = job
+    end
+
 end
+
+p1 = Person.new("Goldie", "Student")
